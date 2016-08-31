32add1c273071bc36d644d652480c46a4b96a839
ivanjqk <ivanjqk@gmail.com>
2016-08-31 17:18:57 +0100

    Breaking travis test.

--- src/main/java/ar/fiuba/tdd/template/Main.java
+++ src/main/java/ar/fiuba/tdd/template/Main.java
@@ -2,6 +2,5 @@ package ar.fiuba.tdd.template;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is just a template project that checks travis and tags the github push and changes the travis image on README.md.");
-    }
-}
+        System.out.println("Now I break it.");
+    }}
